f20685c5b0b9770d30ab133c7553cfa0b0f68c9d
rosstobia <rosstobia@hotmail.com>
2016-12-09 11:26:04 +0000

    basics added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var rows = 8;
+var columns = 8;
+var $row = $("<div />", {
+    class: 'row'
+});
+var $square = $("<div />", {
+    class: 'square'
+});
+
+$(document).ready(function () {
+    //add columns to the the temp row object
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i < rows; i++) {
+        $("#wrapper").append($row.clone());
+    }
+});
